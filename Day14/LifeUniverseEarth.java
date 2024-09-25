7c57e223502b88229ae7aa34aeb5bc4f53f285b1
Ayush Singh Tomar <ayushtomarrudra@gmail.com>
2024-09-25 22:13:35 +0530

    Day14

---
+++ Day14/LifeUniverseEarth.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+class TestClass {
+    public static void main(String args[] ) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int flag=1;
+        while(flag ==1)
+        {
+          int n=Integer.parseInt(br.readLine());
+          if(n==42)
+          {
+            flag=0;
+          }
+          else
+          {
+            System.out.println(n);
+          }
+        }
+
+    }
+}
