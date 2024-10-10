5d1a62105e0c72262a1777ff18b6bca4d06e4ed6
Ayush Singh Tomar <ayushtomarrudra@gmail.com>
2024-10-10 23:47:14 +0530

    day30

---
+++ Day30/Alice.java
@@ -0,0 +1,33 @@
+// #include<stdio.h>
+
+// #include<math.h>
+
+// int main()
+
+// {
+
+// long long int n;
+
+// int nu,p=1,ans=0;
+
+// scanf("%lld",&n);
+
+// nu=sqrt(n);
+
+// while(nu--)
+
+// {
+
+//     if((n+p*p)%(2*p)==0)
+
+//     ans++;
+
+//     p++;
+
+// }
+
+// printf("%d",ans);
+
+//     return 0;
+
+// }
